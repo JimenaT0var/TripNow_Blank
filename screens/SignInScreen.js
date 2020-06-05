b4c186c8dd229fd2ba82a1a2fe5c65f0fe69f966
ITICIFBM <bonifacio@excite.com.mx>
2020-06-05 16:52:43 -0500

    cambio iniciarsessionFBmm3

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -61,7 +61,7 @@ const SignInScreen = ( { navigation } ) => {
         <View style={styles.container}>
             <StatusBar backgroundColor="#496EF9" barStyle="light-content" />
             <View style={styles.header}>
-                <Text style={styles.text_header}>Iniciar SessionFBM</Text>
+                <Text style={styles.text_header}>Iniciar SessionFBMMM</Text>
             </View>
             <Animatable.View
                 animation="fadeInUpBig"
